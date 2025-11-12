2e824933bd0de2e3835e26bd78baf4f229e6daaa
Chava-Sai <128317122+Chava-Sai@users.noreply.github.com>
2025-11-12 03:47:40 +0000

    Deploying to gh-pages from @ Chava-Sai/Chava-Sai.github.io@5421ba99f80c4e9b14a970aff3a44ea81ab2f53e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,14 +430,14 @@ ninja.data = [{
           description: "Multilingual hate-speech and fake-news detection using QLoRA and Retrieval-Augmented Generation (RAG).",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-fake-and-hate-speech-detection-in-multilingual-social-media-text",
+          title: 'Fake and Hate Speech Detection in Multilingual Social Media Text',
+          description: "Multi-task learning model for detecting fake news and hate speech in multilingual, code-mixed text using embeddings, clustering, and shared neural layers.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-boston-mbta-bus-equity-amp-reliability-cs506-final-project",
+          title: 'Boston MBTA Bus Equity &amp;amp; Reliability (CS506 Final Project)',
+          description: "Clustering MBTA bus routes to analyze reliability and equity using ridership, arrival–departure, and 2024 survey data.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
